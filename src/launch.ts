39a5e074d7f869c4aa8bcb3f54b904aa5193f8b8
rutan <ru_shalm@hazimu.com>
2022-08-06 14:42:01 +0900

    set random seed

--- src/launch.ts
+++ src/launch.ts
@@ -3,12 +3,15 @@ import { SceneManager } from './SceneManager';
 export interface LaunchParameter {
   mode?: 'single' | 'ranking' | 'multi';
   totalTimeLimit?: number;
-  gameTimeLimit?: number;
+  randomSeed?: number;
 }
 
 /**
  * ゲーム起動時に呼び出される関数
  */
-export function launch(_params: LaunchParameter) {
+export function launch(params: LaunchParameter) {
+  // 乱数のシード値が指定されている場合はセットする
+  if (params.randomSeed !== undefined) g.game.random.seed = params.randomSeed;
+
   SceneManager.changeScene('title');
 }
