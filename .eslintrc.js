829704664325e96d0241b9aec65be6180531d3a1
rutan <ru_shalm@hazimu.com>
2022-08-06 15:14:37 +0900

    fix prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,6 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
   },
 };
