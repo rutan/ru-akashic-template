da5dc851ea80050a45822be7013c6e9623587b52
rutan <ru_shalm@hazimu.com>
2023-09-28 02:31:07 +0900

    rename public to static

--- scripts/archive.cjs
+++ scripts/archive.cjs
@@ -1,45 +1,14 @@
 const path = require('path');
-const cpx = require('cpx2');
 const mkdirp = require('mkdirp');
-const rimraf = require('rimraf');
 const packageJSON = require('../package.json');
 const { ConsoleLogger } = require('@akashic/akashic-cli-commons');
-const { promiseExportHTML } = require('@akashic/akashic-cli-export/lib/html/exportHTML.js');
 const { promiseExportZip } = require('@akashic/akashic-cli-export/lib/zip/exportZip.js');
 const deployZip = require('@rutan/deployment-zip');
 
 const ROOT_DIR = path.resolve(__dirname, '..');
 
-async function buildForWeb(source, publicDir, output) {
-  const tmpDir = path.resolve(ROOT_DIR, 'tmp', 'build');
-  rimraf.sync(tmpDir);
-  mkdirp.sync(tmpDir);
-
-  const logger = new ConsoleLogger();
-  await promiseExportHTML({
-    cwd: ROOT_DIR,
-    source,
-    force: true,
-    quiet: false,
-    output: tmpDir,
-    logger,
-    strip: false,
-    minify: false,
-    bundle: false,
-    magnify: true,
-    injects: [],
-    unbundleText: true,
-    omitUnbundledJs: false,
-    compress: false,
-    // ファイル名ハッシュを有効にすると sandbox.config.js を連れていけず、
-    // autoSendEventName が反映されなくなるため無効にする
-    hashLength: 0,
-    autoSendEventName: 'localLaunch',
-  });
-
-  cpx.copySync(path.resolve(publicDir, '**', '*'), tmpDir);
-
-  await deployZip.deploy(tmpDir, {
+async function archiveForWeb(publicDir, output) {
+  await deployZip.deploy(publicDir, {
     output,
     ignore: [
       // OS
@@ -53,7 +22,7 @@ async function buildForWeb(source, publicDir, output) {
   });
 }
 
-async function buildForNicoLive(source, dest) {
+async function archiveForNicoLive(source, dest) {
   const logger = new ConsoleLogger();
   await promiseExportZip({
     cwd: ROOT_DIR,
@@ -90,14 +59,14 @@ async function buildForNicoLive(source, dest) {
     packageJSON.version,
   ].join('');
 
-  const source = path.resolve(ROOT_DIR, 'game');
   const publicDir = path.resolve(ROOT_DIR, 'public');
+  const source = path.resolve(ROOT_DIR, 'game');
   const distDir = path.resolve(ROOT_DIR, '_dist', outputName);
 
   mkdirp.sync(distDir);
 
   await Promise.all([
-    buildForWeb(source, publicDir, path.resolve(distDir, `web_${outputName}.zip`)),
-    buildForNicoLive(source, path.resolve(distDir, `nicolive_${outputName}.zip`)),
+    archiveForWeb(publicDir, path.resolve(distDir, `web_${outputName}.zip`)),
+    archiveForNicoLive(source, path.resolve(distDir, `nicolive_${outputName}.zip`)),
   ]);
 })();
---
+++ scripts/buildForWeb.cjs
@@ -0,0 +1,45 @@
+const path = require('path');
+const cpx = require('cpx2');
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+const { ConsoleLogger } = require('@akashic/akashic-cli-commons');
+const { promiseExportHTML } = require('@akashic/akashic-cli-export/lib/html/exportHTML.js');
+
+const ROOT_DIR = path.resolve(__dirname, '..');
+
+async function buildForWeb(source, staticDir, distDir) {
+  const logger = new ConsoleLogger();
+  await promiseExportHTML({
+    cwd: ROOT_DIR,
+    source,
+    force: true,
+    quiet: false,
+    output: distDir,
+    logger,
+    strip: false,
+    minify: false,
+    bundle: false,
+    magnify: true,
+    injects: [],
+    unbundleText: true,
+    omitUnbundledJs: false,
+    compress: false,
+    // ファイル名ハッシュを有効にすると sandbox.config.js を連れていけず、
+    // autoSendEventName が反映されなくなるため無効にする
+    hashLength: 0,
+    autoSendEventName: 'localLaunch',
+  });
+
+  cpx.copySync(path.resolve(staticDir, '**', '*'), distDir);
+}
+
+(async () => {
+  const source = path.resolve(ROOT_DIR, 'game');
+  const staticDir = path.resolve(ROOT_DIR, 'static');
+  const distDir = path.resolve(ROOT_DIR, 'public');
+
+  rimraf.sync(distDir);
+  mkdirp.sync(distDir);
+
+  await buildForWeb(source, staticDir, distDir);
+})();
