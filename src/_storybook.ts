21c6c83ba2b6b1b1e5ddcf191b63a2bcc8eacf54
rutan <ru_shalm@hazimu.com>
2024-04-15 02:13:09 +0900

    fix mount for storybook

--- src/_storybook.ts
+++ src/_storybook.ts
@@ -1,31 +1,36 @@
 import * as AE from '@akashic/akashic-engine-standalone';
 import * as gameJson from '../game/game.json';
 
-export function mount(func: (scene: g.Scene) => g.E) {
-  const canvas = document.createElement('canvas');
+/**
+ * Storybookのrenderに渡す関数を生成する
+ * @param mountFunc
+ */
+export function mount<T>(mountFunc: (params: T) => g.E) {
+  return (params: T) => {
+    const canvas = document.createElement('canvas');
 
-  const destroyFunc = AE.initialize({
-    canvas,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    configuration: gameJson as any,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    mainFunc(g: any) {
-      const scene = new g.Scene({
-        game: g.game,
-        assetIds: [],
-        assetPaths: ['/assets/**/*'],
-      });
-      scene.onLoad.addOnce(function () {
-        const entity = func(scene);
-        scene.append(entity);
-      });
-      g.game.pushScene(scene);
-    },
-  });
+    const destroyFunc = AE.initialize({
+      canvas,
+      configuration: gameJson as AE.GameConfiguration,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      mainFunc(g: any) {
+        const scene = new g.Scene({
+          game: g.game,
+          assetIds: [],
+          assetPaths: ['/assets/**/*'],
+        });
+        scene.onLoad.addOnce(function () {
+          const entity = mountFunc({ ...params, scene });
+          scene.append(entity);
+        });
+        g.game.pushScene(scene);
+      },
+    });
 
-  window.addEventListener('beforeunload', () => {
-    destroyFunc();
-  });
+    window.addEventListener('beforeunload', () => {
+      destroyFunc();
+    });
 
-  return canvas;
+    return canvas;
+  };
 }
