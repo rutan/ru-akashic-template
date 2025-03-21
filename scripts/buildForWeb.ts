c8f84eabb504eda3b586694d43e3f1c6d97e5fa6
rutan <ru_shalm@hazimu.com>
2025-03-21 18:02:07 +0900

    enable hash options
    https://github.com/akashic-games/akashic-cli/pull/1363

--- scripts/buildForWeb.ts
+++ scripts/buildForWeb.ts
@@ -1,19 +1,9 @@
 import { cp, mkdir, rm } from 'node:fs/promises';
 import * as path from 'node:path';
-import { ConsoleLogger } from '@akashic/akashic-cli-commons';
 import { promiseExportHTML } from '@akashic/akashic-cli-export/lib/html/exportHTML.js';
+import { CustomLogger } from './modules/CustomLogger';
 
 const INJECT_HTML_NAME = 'inject.html';
-
-class CustomLogger extends ConsoleLogger {
-  // biome-ignore lint/suspicious/noExplicitAny: AkashicEngine自体の型定義が any のため
-  warn(message: string, cause?: any) {
-    if (message.startsWith('The following ES5 syntax errors exist')) return;
-    if (message.startsWith('Non-ES5 syntax found')) return;
-
-    super.warn(message, cause);
-  }
-}
 
 async function buildForWeb(source: string, staticDir: string, distDir: string) {
   const logger = new CustomLogger();
@@ -32,9 +22,7 @@ async function buildForWeb(source: string, staticDir: string, distDir: string) {
     unbundleText: true,
     omitUnbundledJs: false,
     compress: false,
-    // ファイル名ハッシュを有効にすると sandbox.config.js を連れていけず、
-    // autoSendEventName が反映されなくなるため無効にする
-    hashLength: 0,
+    hashLength: 20,
     autoSendEventName: 'localLaunch',
   });
 
